f898fa97a285a3eb8ddc00f8f32d8a6bd0ec43a5
ngadiono <ondoelratz@gmail.com>
2018-07-22 23:42:46 +0700

    adding firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAqdzzfk_APLyuBJ5oA5BD2Pn1Gk1I_sw8",
+      authDomain: "authapp-d3dae.firebaseapp.com",
+      databaseURL: "https://authapp-d3dae.firebaseio.com",
+      projectId: "authapp-d3dae",
+      storageBucket: "authapp-d3dae.appspot.com",
+      messagingSenderId: "989651238196"
+    });
+  }
+
   render() {
     return (
       <View>
