84a2fbb3b66aabb1fc8ffe0f690631ac9882f1a0
ngadiono <ondoelratz@gmail.com>
2018-07-22 23:56:56 +0700

    creating layout forms

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -19,7 +20,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication"></Header>
-        <Text>ini app</Text>
+        <LoginForm />
       </View>
     );
   }
